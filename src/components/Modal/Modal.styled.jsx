073c603f57e990e27af0f2bb4ac11bdf149e690c
ruudyaa <martarudnytska26@gmail.com>
2023-06-12 02:00:37 +0300

    deletes unnecessary styles.

--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -32,38 +32,3 @@ export const ModalContent = styled.div`
     0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
 `;
 
-export const ModalDescr = styled.p`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  margin: 0 auto;
-  padding-right: ${props => props.theme.space[4]}px;
-  padding-left: ${props => props.theme.space[4]}px;
-  padding-top: ${props => props.theme.space[4]}px;
-  padding-bottom: ${props => props.theme.space[4]}px;
-
-  width: 100%;
-  
-  color: ${props => props.theme.colors.white};
-  background-color: rgba(185, 228, 201, 0.5);
-  box-shadow: 0px -2px 4px 1px rgba(0, 0, 0, 0.2),
-    0px -4px 5px 0px rgba(0, 0, 0, 0.14), 0px -1px 10px 0px rgba(0, 0, 0, 0.12);
-
-  backdrop-filter: blur(5.5px);
-
-  font-family: ${props => props.theme.fonts.monospace}
-  font-size: ${props => props.theme.fontSizes.m};
-  text-align: center;
-`;
-
-export const ModalPicture = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  object-position: center;
-`;
